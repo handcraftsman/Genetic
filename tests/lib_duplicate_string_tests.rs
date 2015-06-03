7ca001f2d3ddd790cc86c8efada6708c23eb7267
Clinton <sheppard@cs.unm.edu>
2015-06-03 11:59:43 -0500

    remove unused variable

--- tests/lib_duplicate_string_tests.rs
+++ tests/lib_duplicate_string_tests.rs
@@ -13,16 +13,16 @@ mod tests {
         let gene_set = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!.";
         let target = "Not all those who wander are lost.";
 
-        let wrapped_display = |candidate: &genetic::Individual| {display_duplicate_string(&candidate, target, start);};
+        let wrapped_display = |candidate: &genetic::Individual| {display_duplicate_string(&candidate, start);};
         let wrapped_get_fitness = |candidate: &String| -> usize {get_duplicate_string_fitness(&candidate, target)};
     
         let best = genetic::get_best(wrapped_get_fitness, wrapped_display, target.len(), target.len(), gene_set);
-        display_duplicate_string(&best, target, start);
+        display_duplicate_string(&best, start);
         println!("Total time: {}", start.to(PreciseTime::now()));
         assert_eq!(best.genes, target);
     }
     
-    fn display_duplicate_string(candidate: &genetic::Individual, target: &str, start: PreciseTime) {
+    fn display_duplicate_string(candidate: &genetic::Individual, start: PreciseTime) {
         let now = PreciseTime::now();
         let elapsed = start.to(now);
         println!("{}\t{}\t{}", candidate.genes, candidate.fitness, elapsed);
